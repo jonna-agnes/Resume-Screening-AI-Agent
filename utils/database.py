90d0619653e273dbf138e86bf21f72536f418dd6
Jonna Agnes J <164525825+jonna-agnes@users.noreply.github.com>
2025-11-30 16:38:08 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,15 +1,23 @@
 import gspread
 from datetime import datetime
 import streamlit as st
+import json
 from google.oauth2.service_account import Credentials
 
 def connect_sheet():
-    credentials = Credentials.from_service_account_info(st.secrets["service_account"])
+    # Load credentials from Streamlit Secrets
+    service_account_info = json.loads(json.dumps(st.secrets["service_account"]))
+
+    # Create credentials object
+    credentials = Credentials.from_service_account_info(service_account_info)
+
+    # Authorize the client
     gc = gspread.authorize(credentials)
+
+    # Connect to Google Sheet
     sheet = gc.open_by_key(st.secrets["GOOGLE_SHEET_ID"]).sheet1
+
     return sheet
-
-
 
 def save_ranked_results(results):
     sheet = connect_sheet()
@@ -59,4 +67,5 @@ def save_ranked_results(results):
             row["Similarity"],
             row["Timestamp"]
         ])
+
 
